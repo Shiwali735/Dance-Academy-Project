83f8d348ed03366b5dd637087268dd72dfc7c99f
Shiwali <shiwalibarnwal@gmail.com>
2020-12-07 14:47:40 +0530

    Mesage("commit on master")

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser")
+mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true});
 const port = 8000;
 
+//Define mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    address: String,
+    email: String,
+    desc: String,
+    about: String
+  });
+
+  const contact = mongoose.model('contact', contactSchema);
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static'))// for serving static files
 app.use(express.urlencoded())//for 26th line for parsing
@@ -18,6 +32,13 @@ res.status(200).render('home.pug', params);
 app.get('/contact', (req, res)=>{
     const params = { }
 res.status(200).render('contact.pug', params);
+})
+app.post('/contact', (req, res)=>{
+    var myData = new contact(req.body);
+    myData.save().then(()=>{
+    res.send("This item has been saved to the database");
+    }).catch(()=>{
+    res.status(400).send("item was not saved to the databse");})
 })
 app.get('/index', (req, res)=>{
     const params = { }
